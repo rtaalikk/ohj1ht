7e9bf9dd99da20c497442bdb5386a7d3c8fa90e2
rtaalikk <roni.taalikka@gmail.com>
2024-04-22 11:36:15 +0300

    Harjoitustyo ja kuva

---
+++ HarjoitusTyo/Tasohyppelypeli/Tasohyppelypeli.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+
+namespace Tasohyppelypeli;
+
+/// @author Omanimi
+/// @version 22.04.2024
+/// <summary>
+/// 
+/// </summary>
+public class Tasohyppelypeli : PhysicsGame
+{
+    public override void Begin()
+    {
+        // Kirjoita ohjelmakoodisi tähän
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+}
